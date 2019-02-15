4d9dc7c7a3b227e8ca2a243e0d6cd1a5315cbd23
h <h@DESKTOP-DDS6EO6>
2019-02-15 17:27:42 +0800

    Revert "Incorporated averaging of counts over iteration"
    This reverts commit 54eff875e2435dfa24fefcde9b00a6ec1949ae04.

--- MetaModelCalibration/src/main/java/ust/hk/praisehk/metamodelcalibration/matsimIntegration/AnaModelControlerListener.java
+++ MetaModelCalibration/src/main/java/ust/hk/praisehk/metamodelcalibration/matsimIntegration/AnaModelControlerListener.java
@@ -1,11 +1,8 @@
 package ust.hk.praisehk.metamodelcalibration.matsimIntegration;
 
-import java.util.ArrayList;
 import java.util.Map;
 
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.controler.events.AfterMobsimEvent;
 import org.matsim.core.controler.events.BeforeMobsimEvent;
@@ -24,7 +21,6 @@ import com.google.inject.name.Named;
 import dynamicTransitRouter.fareCalculators.FareCalculator;
 import ust.hk.praisehk.metamodelcalibration.analyticalModel.AnalyticalModel;
 import ust.hk.praisehk.metamodelcalibration.measurements.Measurements;
-import ust.hk.praisehk.metamodelcalibration.measurements.MeasurementsWriter;
 
 
 
@@ -47,9 +43,6 @@ public class AnaModelControlerListener implements StartupListener,BeforeMobsimLi
 	
 	private int maxIter;
 	private final Map<String, FareCalculator> farecalc;
-	private int AverageCountOverNoOfIteration=5;
-	private boolean shouldAverageOverIteration=false;
-	private Map<String, Map<Id<Link>, Double>> counts=null;
 	
 	@Inject
 	public AnaModelControlerListener(Scenario scenario,AnalyticalModel sueAssignment, 
@@ -80,39 +73,13 @@ public class AnaModelControlerListener implements StartupListener,BeforeMobsimLi
 	
 
 	public void notifyAfterMobsim(AfterMobsimEvent event) {
-		if(this.shouldAverageOverIteration) {
-			int counter=event.getIteration();
-			if(counter>this.maxIter-5) {
-				if(this.counts==null) {
-					counts=this.pcuVolumeCounter.geenerateLinkCounts();
-				}else {
-					Map<String,Map<Id<Link>,Double>> newcounts=this.pcuVolumeCounter.geenerateLinkCounts();
-					for(String s:this.counts.keySet()) {
-						for(Id<Link> lId:this.counts.get(s).keySet()) {
-							counts.get(s).put(lId, counts.get(s).get(lId)+newcounts.get(s).get(lId));
-						}
-					}
-				}
-			}
-			if(counter==this.maxIter) {
-				for(String s:this.counts.keySet()) {
-					for(Id<Link> lId:this.counts.get(s).keySet()) {
-						counts.get(s).put(lId, counts.get(s).get(lId)/this.AverageCountOverNoOfIteration);
-					}
-				}
-				Measurements m=storage.getCalibrationMeasurements().clone();
-				m.updateMeasurements(counts);
-				//new MeasurementsWriter(m).write();
-				this.storage.storeMeasurements(this.currentParam.getParam(), m);
-			}
-		}else {
+		
 		int counter=event.getIteration();
-			if(counter==this.maxIter) {
-				Measurements m=storage.getCalibrationMeasurements().clone();
-				m.updateMeasurements(this.pcuVolumeCounter.geenerateLinkCounts());
-				//m.writeCSVMeasurements(fileLoc);
-				this.storage.storeMeasurements(this.currentParam.getParam(), m);
-			}
+		if(counter==this.maxIter) {
+			Measurements m=storage.getCalibrationMeasurements().clone();
+			m.updateMeasurements(this.pcuVolumeCounter.geenerateLinkCounts());
+			//m.writeCSVMeasurements(fileLoc);
+			this.storage.storeMeasurements(this.currentParam.getParam(), m);
 		}
 		
 	}
@@ -136,23 +103,5 @@ public class AnaModelControlerListener implements StartupListener,BeforeMobsimLi
 
 		
 	}
-
-	public int getAverageCountOverNoOfIteration() {
-		return AverageCountOverNoOfIteration;
-	}
-
-	public void setAverageCountOverNoOfIteration(int averageCountOverNoOfIteration) {
-		AverageCountOverNoOfIteration = averageCountOverNoOfIteration;
-	}
-
-	public boolean isShouldAverageOverIteration() {
-		return shouldAverageOverIteration;
-	}
-
-	public void setShouldAverageOverIteration(boolean shouldAverageOverIteration) {
-		this.shouldAverageOverIteration = shouldAverageOverIteration;
-	}
-	
-	
-	
+		
 }
