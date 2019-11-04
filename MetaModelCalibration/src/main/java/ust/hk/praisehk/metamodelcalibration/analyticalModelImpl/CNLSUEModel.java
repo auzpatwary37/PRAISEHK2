839085ae75c94a8ba900455313d131f9ab235426
auzpa <auzpa@10.79.156.205>
2019-11-04 23:21:52 +0800

    change the param limit for the internal parameter calibration

--- MetaModelCalibration/src/main/java/ust/hk/praisehk/metamodelcalibration/analyticalModelImpl/CNLSUEModel.java
+++ MetaModelCalibration/src/main/java/ust/hk/praisehk/metamodelcalibration/analyticalModelImpl/CNLSUEModel.java
@@ -199,10 +199,10 @@ public class CNLSUEModel implements AnalyticalModel{
 	protected void loadAnalyticalModelInternalPamamsLimit() {
 		this.AnalyticalModelParamsLimit.put(CNLSUEModel.LinkMiuName, new Tuple<Double,Double>(0.0075,0.25));
 		this.AnalyticalModelParamsLimit.put(CNLSUEModel.ModeMiuName, new Tuple<Double,Double>(0.01,0.5));
-		this.AnalyticalModelParamsLimit.put(CNLSUEModel.BPRalphaName, new Tuple<Double,Double>(0.10,0.20));
-		this.AnalyticalModelParamsLimit.put(CNLSUEModel.BPRbetaName, new Tuple<Double,Double>(3.,5.));
-		this.AnalyticalModelParamsLimit.put(CNLSUEModel.TransferalphaName, new Tuple<Double,Double>(0.25,0.75));
-		this.AnalyticalModelParamsLimit.put(CNLSUEModel.TransferbetaName, new Tuple<Double,Double>(0.75,1.5));
+		this.AnalyticalModelParamsLimit.put(CNLSUEModel.BPRalphaName, new Tuple<Double,Double>(0.10,4.));
+		this.AnalyticalModelParamsLimit.put(CNLSUEModel.BPRbetaName, new Tuple<Double,Double>(1.,15.));
+		this.AnalyticalModelParamsLimit.put(CNLSUEModel.TransferalphaName, new Tuple<Double,Double>(0.25,5.));
+		this.AnalyticalModelParamsLimit.put(CNLSUEModel.TransferbetaName, new Tuple<Double,Double>(0.75,4.));
 	}
 	
 	
